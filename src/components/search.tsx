b3c2580ac030979a1b7369bf68c108deb402315d
lucaszebre <yakirisk@gmail.com>
2023-11-13 06:01:00 +0100

    sideBar work well now about to add the animation

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -8,10 +8,8 @@ import {
     SheetTrigger,
   } from "@/components/ui/sheet"
 import { ReactNode } from "react";
-import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Separator } from "./ui/separator";
-import { useStore } from "@/store/zus";
 
 
 interface Props {
@@ -19,15 +17,11 @@ interface Props {
   }
   
   const Search: React.FC<Props> = ({ children }) => {
-    const { setSide, side } = useStore()
 
   return (
     <Sheet>
         <SheetTrigger>
-            <Button onClick={()=>setSide('search')} variant="ghost" className="w-full h-full md:h-[50px]  justify-center md:justify-start gap-5">
-                {side==='search'? <svg aria-label="Découvrir" className="hover:w-[26px] hover:h-[26px] x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Découvrir</title><path d="M18.5 10.5a8 8 0 1 1-8-8 8 8 0 0 1 8 8Z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"></path><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" x1="16.511" x2="21.643" y1="16.511" y2="21.643"></line></svg> : <svg aria-label="Découvrir" className="hover:w-[26px] hover:h-[26px] x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Découvrir</title><path d="M19 10.5A8.5 8.5 0 1 1 10.5 2a8.5 8.5 0 0 1 8.5 8.5Z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="16.511" x2="22" y1="16.511" y2="22"></line></svg>      }
-                {side!=='search' ? <p className={`${side=='search'?'font-bold':''} xl:flex  hidden`}>Search</p>:null}
-            </Button>
+            {children}
         </SheetTrigger>
         <SheetContent>
             <SheetHeader>
--- src/store/zus.tsx
+++ src/store/zus.tsx
@@ -3,9 +3,13 @@ import { create } from 'zustand'
 type Store = {
     side: string;
     setSide: (newSide: string) => void;
+    short: boolean;
+    setShort: (newSide: boolean) => void;
 }
 
 export const useStore = create<Store>((set) => ({
   side: 'home',
   setSide: (newSide) => set(() => ({ side: newSide })),
+  short: false,
+  setShort: (newShort) => set(() => ({ short: newShort }))
 }));
