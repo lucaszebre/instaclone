1188deebfb5233ad1b578e10d2bbb090038c5bce
lucaszebre <yakirisk@gmail.com>
2023-12-17 02:44:47 +0100

    add redis

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from '@prisma/client'
-
+import {Redis} from '@upstash/redis'
 const prisma = new PrismaClient()
+
+export const db = new Redis({
+    url: 'https://eu2-subtle-ram-32443.upstash.io',
+    token: 'AX67ACQgOWFlMjc2MDYtZjE1NC00MmY2LThhNDEtMWNhNGQwM2VjZDFlOGE1NzMyMWJiMjQwNDQwNDg5ZTMyYjlhODIxYzZkNGQ=',
+  })
 
 export default prisma
