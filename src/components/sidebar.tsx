e3badd7fce983858f0aa8a2123c4982994e5b500
lucaszebre <yakirisk@gmail.com>
2023-12-21 11:03:33 +0100

    chat app in coming

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -109,11 +109,11 @@ const pathname = usePathname()
 
                 
                     <Button asChild  onClick={() => handleSidebarToggle('messages')} variant="ghost" className="md:w-full h-full md:h-[50px]  justify-center md:justify-start gap-5">
-                      <Link href={'/inbox'}>
+                      <Link href={'/direct'}>
                       <Tool name="messages">
-                        {pathname==='/inbox'   ?<svg aria-label="Direct" className="hover:w-[26px] hover:h-[26px] x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Direct</title><path d="M22.91 2.388a.69.69 0 0 0-.597-.347l-20.625.002a.687.687 0 0 0-.482 1.178L7.26 9.16a.686.686 0 0 0 .778.128l7.612-3.657a.723.723 0 0 1 .937.248.688.688 0 0 1-.225.932l-7.144 4.52a.69.69 0 0 0-.3.743l2.102 8.692a.687.687 0 0 0 .566.518.655.655 0 0 0 .103.008.686.686 0 0 0 .59-.337L22.903 3.08a.688.688 0 0 0 .007-.692" fill-rule="evenodd"></path></svg>:                <svg aria-label="Direct" className="hover:w-[26px] hover:h-[26px] x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Direct</title><line fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="22" x2="9.218" y1="3" y2="10.083"></line><polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></polygon></svg>}
+                        {pathname==='/direct'   ?<svg aria-label="Direct" className="hover:w-[26px] hover:h-[26px] x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Direct</title><path d="M22.91 2.388a.69.69 0 0 0-.597-.347l-20.625.002a.687.687 0 0 0-.482 1.178L7.26 9.16a.686.686 0 0 0 .778.128l7.612-3.657a.723.723 0 0 1 .937.248.688.688 0 0 1-.225.932l-7.144 4.52a.69.69 0 0 0-.3.743l2.102 8.692a.687.687 0 0 0 .566.518.655.655 0 0 0 .103.008.686.686 0 0 0 .59-.337L22.903 3.08a.688.688 0 0 0 .007-.692" fill-rule="evenodd"></path></svg>:                <svg aria-label="Direct" className="hover:w-[26px] hover:h-[26px] x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Direct</title><line fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="22" x2="9.218" y1="3" y2="10.083"></line><polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></polygon></svg>}
                       </Tool>
-                    <p className={`${pathname==='/inbox'   ?'font-bold':''} ${short? 'hidden' : 'xl:flex  hidden'}`}>Messages</p>
+                    <p className={`${pathname==='/direct'   ?'font-bold':''} ${short? 'hidden' : 'xl:flex  hidden'}`}>Messages</p>
                       </Link>
                       
                     </Button>
