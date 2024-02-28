8458ec37ced129cbf72b88abb4ce3ecfb0940551
lucaszebre <yakirisk@gmail.com>
2024-02-28 06:30:36 +0100

    change in dialogPost

--- src/components/dialogPost.tsx
+++ src/components/dialogPost.tsx
@@ -93,7 +93,7 @@ const DialogPost: React.FC<Props> = ({children,id,likes,avatarurl,username,image
                     {username} . {date}
                 </span>
             </div>
-           <FeedOption>
+           <FeedOption id={id}>
            <svg aria-label="More options" className="x1lliihq x1n2onr6 x5n08af" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>More options</title><circle cx="12" cy="12" r="1.5"></circle><circle cx="6" cy="12" r="1.5"></circle><circle cx="18" cy="12" r="1.5"></circle></svg>
            </FeedOption>
          
